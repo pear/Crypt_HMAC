fb0e4986e9542115314da86ca22af391c7e20f3b
Michael Gauthier <gauthierm@php.net>
2008-11-03 18:43:28 +0000

    Whitespace and API documentation fixes for Crypt_HMAC. Fixes #14945.
    git-svn-id: http://svn.php.net/repository/pear/packages/Crypt_HMAC/trunk@268204 c90b9560-bf6c-de11-be94-00142212c4b1

--- HMAC.php
+++ HMAC.php
@@ -1,9 +1,9 @@
 <?php
+
 /* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
 
 /**
  * Class to calculate RFC 2104 compliant hashes
- *
  *
  * PHP versions 4 and 5
  *
@@ -21,78 +21,80 @@
  * @license    http://www.php.net/license/3_0.txt  PHP License 3.0
  * @version    CVS: $Id$
  * @link       http://pear.php.net/package/Crypt_HMAC
+ * @link       http://www.ietf.org/rfc/rfc2104.txt
  */
 
-
-
-
 /**
-* Calculates RFC 2104 compliant HMACs
-*
-* @access     public
-* @category   Encryption
-* @package    Crypt_HMAC
-* @author     Derick Rethans <derick@php.net>
-* @author     Matthew Fonda <mfonda@dotgeek.org>
-* @copyright  1997-2005 The PHP Group
-* @license    http://www.php.net/license/3_0.txt  PHP License 3.0
-* @link       http://pear.php.net/package/Crypt_HMAC
-*/   
-class Crypt_HMAC 
+ * Calculates RFC 2104 compliant HMACs
+ *
+ * @category   Encryption
+ * @package    Crypt_HMAC
+ * @author     Derick Rethans <derick@php.net>
+ * @author     Matthew Fonda <mfonda@dotgeek.org>
+ * @copyright  1997-2005 The PHP Group
+ * @license    http://www.php.net/license/3_0.txt  PHP License 3.0
+ * @link       http://pear.php.net/package/Crypt_HMAC
+ * @link       http://www.ietf.org/rfc/rfc2104.txt
+ */
+class Crypt_HMAC
 {
-
     /**
-    * Hash function to use
-    * @var string
-    * @access private
-    */
+     * Hash function to use
+     *
+     * @var string
+     * @access private
+     */
     var $_func;
 
     /**
-    * Inner padded key
-    * @var string
-    * @access private
-    */
+     * Inner padded key
+     *
+     * @var string
+     * @access private
+     */
     var $_ipad;
 
     /**
-    * Outer padded key
-    * @var string
-    * @access private
-    */
+     * Outer padded key
+     *
+     * @var string
+     * @access private
+     */
     var $_opad;
-    
+
     /**
-    * Pack format
-    * @var string
-    * @access private
-    */
+     * Pack format
+     *
+     * @var string
+     * @access private
+     */
     var $_pack;
-    
-    
+
     /**
-    * Constructor
-    * Pass method as first parameter
-    *
-    * @param string $key  Key to use for hash
-    * @param string $func  Hash function used for the calculation
-    * @return void
-    * @access public
-    */
+     * Constructor
+     *
+     * @param string $key  the key to use for hashing.
+     * @param string $func the hash function used for the calculation.
+     *
+     * @return void
+     *
+     * @access public
+     */
     function Crypt_HMAC($key, $func = 'md5')
     {
         $this->setFunction($func);
         $this->setKey($key);
     }
-    
-    
+
     /**
-    * Sets hash function
-    *
-    * @param string $func  Hash function to use
-    * @return void
-    * @access public
-    */
+     * Sets the hash function to use
+     *
+     * @param string $func the hash function to use.
+     *
+     * @return void
+     *
+     * @access public
+     */
     function setFunction($func)
     {
         if (!$this->_pack = $this->_getPackFormat($func)) {
@@ -100,71 +102,72 @@ class Crypt_HMAC
         }
         $this->_func = $func;
     }
-    
-    
+
     /**
-    * Sets key to use with hash
-    * 
-    * NOTE: setFunction must be called before setKey
-    *
-    * @param string $key
-    * @return void
-    * @access public
-    */
+     * Sets key to use with hash
+     *
+     * Note: {@link Crypt_HMAC::setFunction()} must be called before setting
+     * the key.
+     *
+     * @param string $key the key to use.
+     *
+     * @return void
+     *
+     * @access public
+     */
     function setKey($key)
     {
-        /* 
-        * Pad the key as the RFC wishes
-        */
+        // Pad the key as the RFC wishes
         $func = $this->_func;
-        
+
         if (strlen($key) > 64) {
            $key =  pack($this->_pack, $func($key));
         }
         if (strlen($key) < 64) {
             $key = str_pad($key, 64, chr(0));
         }
-        
 
-        /* Calculate the padded keys and save them */
+        // Calculate the padded keys and save them
         $this->_ipad = (substr($key, 0, 64) ^ str_repeat(chr(0x36), 64));
-        $this->_opad = (substr($key, 0, 64) ^ str_repeat(chr(0x5C), 64));
+        $this->_opad = (substr($key, 0, 64) ^ str_repeat(chr(0x5c), 64));
     }
-    
-    
+
     /**
-    * Gets pack formats for specifed hash function
-    *
-    * @param string $func
-    * @return mixed  false if hash function doesnt exist, pack format on success
-    * @access private
-    */
+     * Gets pack formats for specifed hash function
+     *
+     * @param string $func the hash function for which to get the pack format.
+     *
+     * @return mixed the pack format or false if hash function does not exist.
+     *
+     * @access private
+     */
     function _getPackFormat($func)
     {
         $packs = array('md5' => 'H32', 'sha1' => 'H40');
         return isset($packs[$func]) ? $packs[$func] : false;
     }
-    
-    
+
     /**
-    * Hashing function
-    *
-    * @param  string $data          string that will encrypted
-    * @param  bool   $rawOutput     if true, digest is returned in raw binary format
-    * @return string
-    * @access public
-    */
+     * Gets the hash of a string
+     *
+     * @param  string  $data      the string that will be hashed.
+     * @param  boolean $rawOutput if true the message digest is returned in raw
+     *                            binary format.
+     *
+     * @return string the hashed string.
+     *
+     * @access public
+     */
     function hash($data, $rawOutput = false)
     {
         $func = $this->_func;
         if ($rawOutput) {
             return pack('H*', $func($this->_opad . pack($this->_pack,
-$func($this->_ipad . $data))));
+                $func($this->_ipad . $data))));
         }
-        return $func($this->_opad . pack($this->_pack, $func($this->_ipad . $data)));
+        return $func($this->_opad . pack($this->_pack,
+            $func($this->_ipad . $data)));
     }
-
 }
-
 
 ?>
